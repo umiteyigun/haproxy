2b211a747715b551e397474e6b3ea9c245768ed5
mustafa.altinova <exteremewaystogohome@hotmail.com>
2026-05-20 11:14:29 +0300

    Web arayüz eklendi

--- guard/scripts/list-bans.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-echo "=== Banlı IP Listesi (INPUT Chain) ==="
-# Header'ı göster
-sudo iptables -L INPUT -n -v | head -2
-# Sadece DROP olanları göster
-sudo iptables -L INPUT -n -v --line-numbers | grep DROP
-echo "======================================"
